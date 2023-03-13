f500d577090f75325e19bbe977ff8aefde41fcb7
congtan97 <congtan98@gmail.com>
2023-03-13 12:36:17 +0700

    Add validation

--- tantcd/src/main/java/fpt/m2/tantcd/entity/User.java
+++ tantcd/src/main/java/fpt/m2/tantcd/entity/User.java
@@ -2,7 +2,9 @@ package fpt.m2.tantcd.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 
 @Entity
@@ -10,11 +12,15 @@ import lombok.Setter;
 @Setter
 public class User {
     @Id
+    @NotEmpty(message = "User name cannot be blank")
     String userName;
 
+    @NotEmpty
     String email;
 
+    @NotEmpty
     String address;
 
+    @NonNull
     int phoneNumber;
 }
