7f07aa0e55639e686fe7a36b1d646b36aa61658e
congtan97 <congtan98@gmail.com>
2023-03-15 13:48:19 +0700

    Add unit test

--- tantcd/src/test/java/fpt/m2/tantcd/TantcdApplicationTests.java
+++ tantcd/src/test/java/fpt/m2/tantcd/TantcdApplicationTests.java
@@ -1,13 +1,68 @@
 package fpt.m2.tantcd;
 
-import org.junit.jupiter.api.Test;
+import fpt.m2.tantcd.entity.User;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class TantcdApplicationTests {
-
+    private static final String BASE_URL = "http://127.0.0.1:8080/user";
     @Test
-    void contextLoads() {
+    @Order(1)
+    void testCreateUser() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        User userCreate = new User();
+        userCreate.setUserName("congtan97");
+        userCreate.setEmail("congtan98@gmail.com");
+        userCreate.setAddress("Danang");
+        userCreate.setPhoneNumber(12345);
+
+        HttpEntity<User> request = new HttpEntity<>(userCreate);
+        ResponseEntity<User> response = restTemplate.exchange(BASE_URL, HttpMethod.POST,request, User.class);
+        Assertions.assertEquals(response.getStatusCode().value(), 200);
     }
 
+    @Test
+    @Order(2)
+    void testGetUser() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        String userName = "congtan97";
+        User user = restTemplate
+                .getForObject(BASE_URL + "/" + userName, User.class);
+        Assertions.assertNotNull(user.getUserName());
+        Assertions.assertEquals(user.getEmail(), "congtan98@gmail.com");
+    }
+
+    @Test
+    @Order(3)
+    void testUpdateUser() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        User userCreate = new User();
+        userCreate.setUserName("congtan97");
+        userCreate.setEmail("congtan98@gmail.com");
+        userCreate.setAddress("Danang");
+        userCreate.setPhoneNumber(98765);
+
+        HttpEntity<User> request = new HttpEntity<>(userCreate);
+        ResponseEntity<User> response = restTemplate.exchange(BASE_URL, HttpMethod.PUT,request, User.class);
+        Assertions.assertEquals(response.getStatusCode().value(), 200);
+    }
+
+    @Test
+    @Order(4)
+    void testDeleteUser() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        String userName = "congtan97";
+
+        restTemplate.delete(BASE_URL + "/" + userName);
+    }
 }
